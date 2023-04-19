ef2d3f05ff06a5ff3bf016f4299d53aa17a5960b
Art Mills <art@hamcois.com>
2023-04-18 22:34:14 -0500

    Barely there first page.

---
+++ controllers/homepage_controller.go
@@ -0,0 +1,16 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Home(c *gin.Context) {
+	c.HTML(
+		http.StatusOK,
+		"home/index.html",
+		gin.H{
+			"title": "Splattastic",
+		})
+}
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hail2skins/splattastic/controllers"
+	"github.com/hail2skins/splattastic/setup"
+)
+
+func main() {
+	setup.LoadEnv()
+	//	setup.LoadDatabase()
+	serveApplication()
+}
+
+func serveApplication() {
+	r := gin.Default()
+
+	r.LoadHTMLGlob("templates/**/**")
+
+	r.GET("/", controllers.Home)
+
+	r.Static("/css", "./static/css")
+	r.Static("/img", "./static/img")
+	r.Static("/scss", "./static/scss")
+	r.Static("/vendor", "./static/vendor")
+	r.Static("/js", "./static/js")
+	r.StaticFile("/favicon.ico", "./img/favicon.ico")
+
+	log.Println("Server started")
+	r.Run(":8080") // listen and serve on localhost:8080
+}
