07cb6718bc4fcf2469e28d07096ce65405ca5408
Art Mills <art@hamcois.com>
2023-05-20 20:57:30 -0500

    [#185008985] Model updated to make Zip required with validation. Test started for model, not done.

--- models/team.go
+++ models/team.go
@@ -1,6 +1,13 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"errors"
+	"log"
+	"regexp"
+
+	db "github.com/hail2skins/splattastic/database"
+	"gorm.io/gorm"
+)
 
 // Team struct is is the table for teams
 type Team struct {
@@ -9,11 +16,52 @@ type Team struct {
 	Street       string   `json:"street"`
 	Street1      string   `json:"street1"`
 	City         string   `json:"city"`
-	Zip          string   `json:"zip"`
+	Zip          string   `gorm:"not null" json:"zip"`
 	Abbreviation string   `json:"abbreviation"`
 	TeamTypeID   uint64   `gorm:"not null" json:"teamtype_id"`
 	TeamType     TeamType `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"team_type"`
 	Users        []User   `gorm:"many2many:user_teams;association_jointable_foreignkey:user_id;jointable_foreignkey:team_id;" json:"users"`
 	StateID      uint64   `gorm:"not null" json:"state_id"`
 	State        State    `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"state"`
+}
+
+// TeamCreate creates a new team
+// Needs the FK relationship with team_type and needs to
+// be associated with a state. Also needs lots of valication
+func TeamCreate(name string, street string, street1 string, city string, zip string, abbreviation string, teamTypeID uint64, stateID uint64) (*Team, error) {
+	// Check if the associated records exist
+	_, err := TeamTypeShow(teamTypeID)
+	if err != nil {
+		return nil, err
+	}
+	_, err = StateShow(stateID)
+	if err != nil {
+		return nil, err
+	}
+
+	// validate zip code
+	zipRegex := `^\d{5}$|^\d{5}-\d{4}$`
+	match, _ := regexp.MatchString(zipRegex, zip)
+	if !match {
+		return nil, errors.New("Invalid zip code must be in the format 12345 or 12345-1234")
+	}
+
+	team := &Team{
+		Name:         name,
+		Street:       street,
+		Street1:      street1,
+		City:         city,
+		Zip:          zip,
+		Abbreviation: abbreviation,
+		TeamTypeID:   teamTypeID,
+		StateID:      stateID,
+	}
+
+	result := db.Database.Create(team)
+	if result.Error != nil {
+		log.Printf("Error creating team: %v", result.Error)
+		return nil, result.Error
+	}
+
+	return team, nil
 }
---
+++ models/tests/team_create_test.go
@@ -0,0 +1,50 @@
+package models
+
+import (
+	"testing"
+
+	db "github.com/hail2skins/splattastic/database"
+	"github.com/hail2skins/splattastic/models"
+)
+
+// TestTeamCreate tests the TeamCreate function
+func TestTeamCreate(t *testing.T) {
+	// Setup
+	LoadEnv()
+	db.Connect()
+
+	// Create a user type
+	ut1, _ := models.CreateUserType("Test User Type")
+
+	// Create a User
+	user, _ := models.UserCreate("test@example.com", "testpassword", "Test", "User", "testuser", ut1.Name)
+
+	// Create a team type
+	tt1, _ := models.TeamTypeCreate("Test Team Type")
+
+	// Create a state
+	state, _ := models.StateCreate("Test State", "TS")
+
+	// Create a team with required fields
+	team, err := models.TeamCreate("Test Team", "", "", "", "12345", "", uint64(tt1.ID), uint64(state.ID))
+	if err != nil {
+		t.Fatalf("Error creating team: %v", err)
+	}
+
+	// Deferred cleanup
+	defer func() {
+		// Delete the team
+		db.Database.Unscoped().Delete(&team)
+		// Delete the UserMarker
+		var userMarker models.UserMarker
+		db.Database.Unscoped().Where("user_id = ?", user.ID).Delete(&userMarker)
+		// Delete the user
+		db.Database.Unscoped().Delete(&user)
+		// Delete the team type
+		db.Database.Unscoped().Delete(&tt1)
+		// Delete the state
+		db.Database.Unscoped().Delete(&state)
+		// Delete the user type
+		db.Database.Unscoped().Delete(&ut1)
+	}()
+}
